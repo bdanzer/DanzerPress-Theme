c43c0dbbf9106b10840860e17f1db17390ea0646
bdanzer <britt.danzer@gmail.com>
2017-11-10 03:53:45 -0600

    Fixed error with theme stylesheet not being added

--- functions.php
+++ functions.php
@@ -164,6 +164,9 @@ add_action( 'widgets_init', 'danzerpress_widgets_init' );
  */
 function danzerpress_scripts() {
 
+	//Enqueue Main Style Sheet
+	wp_enqueue_style( 'danzerpress-style', get_stylesheet_uri() );
+
 	//wp_enqueue_script( 'danzerpress-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 	wp_enqueue_script( 'danzerpress-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
